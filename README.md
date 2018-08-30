98877a6d95c82d21c78e930aa03523cbea04f33b
piBox <yaremiyroman@gmail.com>
2018-08-30 12:45:25 +0300

    added bot comand to switch lights through the command [/light]

--- README.md
+++ README.md
@@ -246,6 +246,15 @@ $ sudo reboot
 # and test
 $ sudo hwclock -r
 
+
+# smthng related to Telegram bot
 $ sudo pip3 install telegram-send
 $ pip3 install telegram-send
-$
+
+# AttributeError: 'TeleBot' object has no attribute
+$ pip3 uninstall telebot
+$ pip3 uninstall PyTelegramBotAPI
+$ pip3 install PyTelegramBotAPI==2.2.3
+# and it works now!
+
+#PyTelegramBotAPI 3.0 have lot of changes and 3.0 API docs are not there yet. WIP. Use 2.2.3 older version instead and make sure you don't have any other bot api like telebot
--- pyScripts/bot.py
+++ pyScripts/bot.py
@@ -1,8 +1,33 @@
 # -*- coding: utf-8 -*-
 import config
 import telebot
+import RPi.GPIO as GPIO
 
 bot = telebot.TeleBot(config.token)
+
+
+# It is possible that you have more than one
+# script/circuit on the GPIO of your Raspberry Pi.
+# As a result of this, if RPi.GPIO detects that a
+# pin has been configured to something other than
+# the default (input), you get a warning when you
+# try to configure a script. To disable these warnings:
+GPIO.setwarnings(False)
+
+# set up GPIO using BCM numbering
+GPIO.setmode(GPIO.BCM)
+# setup GPIO using Board numbering
+# GPIO.setmode(GPIO.BOARD)
+
+# set up LED pins to OUT MODE
+# for multiple pins control array can be uase as argument
+# example: GPIO.output(lights, 1)
+# will turn on all light pins
+lights = [23, 24]
+GPIO.setup(lights, GPIO.OUT)
+
+light_1 = 23
+light_1 = 24
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
@@ -19,5 +44,15 @@ def send_welcome(message):
 @bot.message_handler(commands=['weather'])
 def send_welcome(message):
     msg = bot.send_message(message.chat.id, 'Тут будет состояние системы')
+
+@bot.message_handler(commands=['light'])
+def send_welcome(message):
+    light_state = GPIO.input(light_1)
+    if light_state == 1:
+        msg = bot.send_message(message.chat.id, 'Выключаем... :(')
+        GPIO.output(lights, 0)
+    else:
+        msg = bot.send_message(message.chat.id, ':) Включаем!!!')
+        GPIO.output(lights, 1)
 
 bot.polling()
