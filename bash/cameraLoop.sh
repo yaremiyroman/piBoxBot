bc0cabf636cbd6b6a053eeca0d1a916ac9ad487c
piBox <yaremiyroman@gmail.com>
2018-08-31 15:11:51 +0300

    added additional photo capture with low size for telegram sending

--- bash/cameraLoop.sh
+++ bash/cameraLoop.sh
@@ -2,10 +2,11 @@
 
 while true;
 sleep 225;
+do
 rm /home/pi/box_recent_photo.jpg;
 sleep 3;
-do raspistill -t 50 -w 640 -h 480 -q 33 --thumb none --exif none -o /home/pi/box_recent_photo.jpg
+raspistill -t 50 -w 640 -h 480 -q 33 --thumb none --exif none -o /home/pi/box_recent_photo.jpg
 sleep 25;
-do raspistill -t 100 -o /home/pi/Pictures/cam/box_$(date +"%d_%m_%y__%H_%M_%S").jpg;
+raspistill -t 100 -o /home/pi/Pictures/cam/box_$(date +"%d_%m_%y__%H_%M_%S").jpg;
 sleep 25;
 done
