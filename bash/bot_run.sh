eb2a9bfa594c944bb1e3ec6d7dfdc503d9606438
piBox <yaremiyroman@gmail.com>
2019-01-25 08:53:36 +0200

    Resolving issue with 8hrs

--- bash/bot_run.sh
+++ bash/bot_run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sleep 50;
+# sleep 50;
 sudo python3 /home/pi/piBoxBot/py/bot.py;
--- bash/cameraLoop.sh
+++ bash/cameraLoop.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
+
 sleep 100;
+
 while true;
-sleep 150;
-do
-#rm /home/pi/box_recent_photo.jpg;
-#sleep 100;
-# raspistill -t 1000 -w 640 -h 480 -q 33 --thumb none -ex auto -awb fluorescent -sh 75  -sa 10 -br 50 -o /home/pi/box_recent_photo.jpg;
-#raspistill -t 1000 -w 640 -h 480 -o /home/pi/box_recent_photo.jpg;
-#sleep 100;
-# raspistill -t 1000 -ex auto -awb fluorescent -sh 75  -sa 10 -br 50 -o /home/pi/Pictures/cam/box_$(date +"%d_%m_%y__%H_%M_%S").jpg;
-raspistill -t 1000 -o /home/pi/Pictures/cam/box_$(date +"%d_%m_%y__%H_%M_%S").jpg;
-sleep 500;
+  do
+  #rm /home/pi/box_recent_photo.jpg;
+  #sleep 100;
+  # raspistill -t 1000 -w 640 -h 480 -q 33 --thumb none -ex auto -awb fluorescent -sh 75  -sa 10 -br 50 -o /home/pi/box_recent_photo.jpg;
+  #raspistill -t 1000 -w 640 -h 480 -o /home/pi/box_recent_photo.jpg;
+  #sleep 100;
+  # raspistill -t 1000 -ex auto -awb fluorescent -sh 75  -sa 10 -br 50 -o /home/pi/Pictures/cam/box_$(date +"%d_%m_%y__%H_%M_%S").jpg;
+  raspistill -q 40 -w 1440 -h 1080 -o /home/pi/Pictures/cam/box_$(date +"%d_%m_%y__%H_%M_%S").jpg;
+  sleep 120;
 done
