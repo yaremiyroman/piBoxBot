cb93e9255a78402aea48980f3cd6b5754b623f54
Roman Y <roman@clearx.io>
2019-01-31 01:21:34 +0200

    telebot disconnect handling

--- py/bot.py
+++ py/bot.py
@@ -181,4 +181,16 @@ def shutdown(message):
 ############################################################################
 ############################################################################
 
-bot.polling(none_stop = True, timeout = 600)
+def telegram_polling():
+    try:
+        bot.polling(none_stop = True, timeout = 600)
+    except:
+        traceback_error_string=traceback.format_exc()
+        with open("Error.Log", "a") as myfile:
+            myfile.write("\r\n\r\n" + time.strftime("%c")+"\r\n<<ERROR polling>>\r\n"+ traceback_error_string + "\r\n<<ERROR polling>>")
+        bot.stop_polling()
+        time.sleep(30)
+        telegram_polling()
+
+if __name__ == '__main__':
+    telegram_polling()
