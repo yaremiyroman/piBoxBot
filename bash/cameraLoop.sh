56dd459b79b208e5ad8a7029cce7a632332b71d6
Roman Y <roman@clearx.io>
2019-05-27 21:29:13 +0300

    Camera loop update

--- bash/cameraLoop.sh
+++ bash/cameraLoop.sh
@@ -6,7 +6,7 @@ while true;
   do
     mkdir -p /home/pi/Pictures/cam/$(date +"%d_%m_%y")
     raspistill -q 40 -w 1440 -h 1080 -o /home/pi/Pictures/cam/$(date +"%d_%m_%y")/box_$(date +"%H_%M_%S").jpg;
-    sleep 600;
+    sleep 60;
 done
 
 # raspistill -t 1000 -w 640 -h 480 -q 33 --thumb none -ex auto -awb fluorescent -sh 75  -sa 10 -br 50 -o /home/pi/box_recent_photo.jpg;
