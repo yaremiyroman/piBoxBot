2618a78f999db9f0bf34608b850a1cb7c29e0ef4
Roman Y <roman@clearx.io>
2019-05-27 22:03:46 +0300

    Camera loop update

--- bash/cameraLoop.sh
+++ bash/cameraLoop.sh
@@ -4,9 +4,14 @@ sleep 30;
 
 while true;
   do
+    sleep 30;
+    mkdir -p /home/pi/Pictures/usbCam/$(date +"%d_%m_%y")
+    fswebcam -r 640x480 /home/pi/Pictures/usbCam/$(date +"%d_%m_%y")/usbCam_$(date +"%H_%M_%S").jpg
+    sleep 60
+
     mkdir -p /home/pi/Pictures/cam/$(date +"%d_%m_%y")
     raspistill -q 40 -w 1440 -h 1080 -o /home/pi/Pictures/cam/$(date +"%d_%m_%y")/box_$(date +"%H_%M_%S").jpg;
-    sleep 60;
+    sleep 600;
 done
 
 # raspistill -t 1000 -w 640 -h 480 -q 33 --thumb none -ex auto -awb fluorescent -sh 75  -sa 10 -br 50 -o /home/pi/box_recent_photo.jpg;
