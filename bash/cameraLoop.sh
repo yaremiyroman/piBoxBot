1c64a1fd71cfd3fc2341b8c8f49acb595e8921d4
Roman Y <roman@clearx.io>
2019-05-27 22:04:55 +0300

    Camera loop update

--- bash/cameraLoop.sh
+++ bash/cameraLoop.sh
@@ -14,6 +14,7 @@ while true;
     sleep 600;
 done
 
+# fswebcam should be apt-get-installed
 # raspistill -t 1000 -w 640 -h 480 -q 33 --thumb none -ex auto -awb fluorescent -sh 75  -sa 10 -br 50 -o /home/pi/box_recent_photo.jpg;
 # raspistill -t 1000 -w 640 -h 480 -o /home/pi/box_recent_photo.jpg;
 # raspistill -t 1000 -ex auto -awb fluorescent -sh 75  -sa 10 -br 50 -o /home/pi/Pictures/cam/box_$(date +"%d_%m_%y__%H_%M_%S").jpg;
