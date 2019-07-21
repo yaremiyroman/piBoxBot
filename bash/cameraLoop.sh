9aabe58ee2ffa9e23b54d60d8d5ce5d772714b15
Roman Y <roman@clearx.io>
2019-07-21 18:01:37 +0300

    cameraloop updates

--- bash/cameraLoop.sh
+++ bash/cameraLoop.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-sleep 30;
+sleep 30
 
-while true;
-  do
-    mkdir -p /home/pi/Pictures/cam/$(date +"%d_%m_%y");
-    sleep 10;
-    raspistill -q 40 -w 1440 -h 1080 -o /home/pi/Pictures/cam/$(date +"%d_%m_%y")/box_$(date +"%H_%M_%S").jpg;
-    sleep 300;
-    raspistill -q 85 -sh 15 -ISO 400 -ex auto -w 2592 -h 1944 -o /home/pi/Pictures/cam/$(date +"%d_%m_%y")/box_$(date +"%H_%M_%S").jpg;
-    sleep 300;
+while true; do
+  mkdir -p /home/pi/Pictures/cam/$(date +"%d_%m_%y")
+  sleep 10
+  raspistill -q 40 -w 1440 -h 1080 -o /home/pi/Pictures/cam/$(date +"%d_%m_%y")/box_$(date +"%H_%M_%S").jpg
+  sleep 300
+  raspistill -q 85 -sh 15 -ISO 400 -ex auto -w 2592 -h 1944 -o /home/pi/Pictures/cam/$(date +"%d_%m_%y")/box_$(date +"%H_%M_%S").jpg
+  sleep 300
 done
