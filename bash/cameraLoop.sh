90f5baf9d0d035c61f86f9a5339c02a4ff23ff15
andrdrx_piData <yaremiyroman@gmail.com>
2019-07-25 09:19:52 +0300

    camera loop updated

--- bash/cameraLoop.sh
+++ bash/cameraLoop.sh
@@ -4,8 +4,10 @@ sleep 30
 
 while true; do
   mkdir -p /home/pi/Pictures/cam/$(date +"%d_%m_%y")
-  sleep 10
+  sleep 5
   file_path=/home/pi/Pictures/cam/$(date +"%d_%m_%y")/box_$(date +"%H_%M_%S").jpg
   raspistill -q 80 -w 1440 -h 1080 -o $file_path
-  sleep 600
+  sleep 5
+  cp $file_path /home/pi/Pictures/cam1.jpg
+  sleep 30
 done
