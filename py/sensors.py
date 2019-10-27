47b4b7f91dafb0e74fe4c450864346da5e96985b
Roman Yaremiy <ryaremiy@deem.com>
2019-10-27 12:49:39 +0200

    sensors service updated

--- py/sensors.py
+++ py/sensors.py
@@ -7,54 +7,21 @@ import Adafruit_DHT
 import RPi.GPIO as GPIO
 from config import config 
 
-#########################################################################
-####################### VARS ###########################################
-#########################################################################
 pause = 10
 
-# DS18b20 = 17
+DHT22_1 = 17
 
-DHT22_1 = 14
-
-# DHT11_1 = 14
-# DHT11_2 = 15
-# DHT11_3 = 18
-# DHT11_4 = 23
-# DHT11_5 = 22
-
-### 1-wire interfacing for ds18b20
-# ds18b20_file = glob.glob('/sys/bus/w1/devices/28*')[0] + '/w1_slave'
-
-#########################################################################
-####################### INIT ###########################################
-#########################################################################
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-# GPIO.setup(DS18b20, GPIO.IN)
-
 GPIO.setup(DHT22_1, GPIO.IN)
 
-# GPIO.setup(DHT11_1, GPIO.IN)
-# GPIO.setup(DHT11_2, GPIO.IN)
-# GPIO.setup(DHT11_3, GPIO.IN)
-# GPIO.setup(DHT11_4, GPIO.IN)
-# GPIO.setup(DHT11_5, GPIO.IN)
-
-# os.system('modprobe w1-gpio')
-# os.system('modprobe w1-therm')
- 
 DB = sqlite3.connect(config.unoClimateDB)
 
 #########################################################################
-####################### MAIN LOOP #######################################
-#########################################################################
 
 while True:
-    #########################################################################
-    ####################### DHT22 ###########################################
-    #########################################################################
-    
+
     ### DHT22_1
     dht22_1_h, dht22_1_t = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, DHT22_1)
 
@@ -64,75 +31,5 @@ while True:
         DB.commit()
     
     time.sleep(pause)
-
-    #########################################################################
-    ####################### DHT11 ###########################################
-    #########################################################################
-    
-    # ### DHT11_1
-    # dht11_1_h, dht11_1_t = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, DHT11_1)
-
-    # if dht11_1_h is not None and dht11_1_t is not None:
-    #     query = "INSERT INTO dht11_1 (t, h) VALUES(" + str(dht11_1_t) + ", " + str(dht11_1_h) + ")"
-    #     DB.execute(query)
-    #     DB.commit()
-    
-    # time.sleep(pause)
-
-    # ### DHT11_2
-    # dht11_2_h, dht11_2_t = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, DHT11_2)
-
-    # if dht11_2_h is not None and dht11_2_t is not None:
-    #     query = "INSERT INTO dht11_2 (t, h) VALUES(" + str(dht11_2_t) + ", " + str(dht11_2_h) + ")"
-    #     DB.execute(query)
-    #     DB.commit()
-                
-    # time.sleep(pause)
-
-    # ### DHT11_3
-    # dht11_3_h, dht11_3_t = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, DHT11_3)
-
-    # if dht11_3_h is not None and dht11_3_t is not None:
-    #     query = "INSERT INTO dht11_3 (t, h) VALUES(" + str(dht11_3_t) + ", " + str(dht11_3_h) + ")"
-    #     DB.execute(query)
-    #     DB.commit()
-                
-    # time.sleep(pause)
-
-    # ### DHT11_4
-    # dht11_4_h, dht11_4_t = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, DHT11_4)
-
-    # if dht11_4_h is not None and dht11_4_t is not None:
-    #     query = "INSERT INTO dht11_4 (t, h) VALUES(" + str(dht11_4_t) + ", " + str(dht11_4_h) + ")"
-    #     DB.execute(query)
-    #     DB.commit()
-                
-    # time.sleep(pause)
-
-    # ### DHT11_5
-    # dht11_5_h, dht11_5_t = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, DHT11_5)
-
-    # if dht11_5_h is not None and dht11_5_t is not None:
-    #     query = "INSERT INTO dht11_5 (t, h) VALUES(" + str(dht11_5_t) + ", " + str(dht11_5_h) + ")"
-    #     DB.execute(query)
-    #     DB.commit()
-                
-    # time.sleep(pause)
-
-    # #########################################################################
-    # ####################### DS18B20 ###########################################
-    # #########################################################################
-    # f = open(ds18b20_file, 'r')
-    # lines = f.readlines()
-    # f.close()
-    
-    # ds18b20_temp = float(lines[1][lines[1].find('t=')+2:]) / 1000.0
-    
-    # if ds18b20_temp is not None:
-    #     query = "INSERT INTO ds18b20 (t) VALUES(" + str(ds18b20_temp) + ")"
-    #     DB.execute(query)
-    #     DB.commit()
-                
-    # time.sleep(pause)
 
 DB.close()
---
+++ sketches/climate/climate.ino
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <DHT.h>
+
+struct sensor {
+  int pin;
+  String label;
+};
+
+const int PAUSE = 500;
+const int REST = 1000;
+
+struct sensor DS18B20 = { 2, "ds18b20" };
+// struct sensor DHT22_1 = { 3, "DHT22_1" };
+struct sensor DHT11_1 = { 4, "DHT11_1" };
+struct sensor DHT11_2 = { 5, "DHT11_2" };
+struct sensor DHT11_3 = { 6, "DHT11_3" };
+struct sensor DHT11_4 = { 7, "DHT11_4" };
+// struct sensor FIRE = {8, "FIRE" };
+// struct sensor LIGHT_1 = { 9, "LIGHT_1" };
+// struct sensor RAIN = { 10, "RAIN" };
+
+struct sensor LIGHT_2 = { 0, "light_2" };
+struct sensor LIQUID = { 1,  "liquid_lvl" };
+struct sensor STEAM = { 2,  "steam_1" };
+struct sensor MOISTURE_1 = { 3, "moisture_1" };
+struct sensor MOISTURE_2 = { 4, "moisture_2" };
+struct sensor MOISTURE_3 = { 5, "moisture_3" };
+// struct sensor HALL = { 6,  "hall" };
+
+OneWire oneWire(DS18B20.pin);
+DallasTemperature sensors(&oneWire);
+DHT dht11_1(DHT11_1.pin, DHT11);
+DHT dht11_2(DHT11_2.pin, DHT11);
+DHT dht11_3(DHT11_3.pin, DHT11);
+DHT dht11_4(DHT11_4.pin, DHT11);
+
+void setup() {
+  Serial.begin(9600);
+
+  sensors.begin();
+
+  dht11_1.begin();
+  dht11_2.begin();
+  dht11_3.begin();
+  dht11_4.begin();
+
+  Serial.println();
+}
+
+void printSensor(String label, float val, float val2 = NAN) {
+  if (isfinitef(val)) {
+    Serial.print(label);
+    Serial.print("=");
+    Serial.print(val);
+    if (isfinitef(val2)) {
+      Serial.print("=");
+      Serial.print(val2);
+    }
+    Serial.print(" || ");
+    delay(PAUSE);
+  }
+}
+
+void loop() {
+  delay(PAUSE);
+  sensors.requestTemperatures();
+
+  // printSensor(DS18B20.label, sensors.getTempCByIndex(0));
+  // printSensor(DHT22_1.label, dht22_1.readTemperature(), dht22_1.readHumidity());
+  //  printSensor(DHT11_1.label, dht11_1.readTemperature(), dht11_1.readHumidity());
+  //  printSensor(DHT11_2.label, dht11_2.readTemperature(), dht11_2.readHumidity());
+  //  printSensor(DHT11_3.label, dht11_3.readTemperature(), dht11_3.readHumidity());
+  //  printSensor(DHT11_4.label, dht11_4.readTemperature(), dht11_4.readHumidity());
+  // printSensor(FIRE.label, digitalRead(FIRE.pin));
+  // printSensor(LIGHT_1.label, digitalRead(LIGHT_1.pin));
+  // printSensor(RAIN.label, digitalRead(RAIN.pin));
+
+  printSensor(MOISTURE_1.label, analogRead(MOISTURE_1.pin));
+  printSensor(MOISTURE_2.label, analogRead(MOISTURE_2.pin));
+  printSensor(MOISTURE_3.label, analogRead(MOISTURE_3.pin));
+  printSensor(STEAM.label, analogRead(STEAM.pin));
+  printSensor(LIQUID.label, analogRead(LIQUID.pin));
+  printSensor(LIGHT_2.label, analogRead(LIGHT_2.pin));
+
+  Serial.println();
+  delay(REST);
+}
