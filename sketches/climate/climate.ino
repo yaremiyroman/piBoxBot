443b86ab320fbde1ef791fe2bf32266e797954e7
Roman Yaremiy <ryaremiy@deem.com>
2019-12-06 17:01:53 +0200

    climate sketch updatead and ready to deploy

--- sketches/climate/climate.ino
+++ sketches/climate/climate.ino
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <DHT.h>
 
 struct sensor {
@@ -7,22 +6,23 @@ struct sensor {
 };
 
 const int PAUSE = 1000;
+const int REST = 250;
 
 struct sensor DHT_1 = { 4, "dht1" };
 struct sensor DHT_2 = { 5, "dht2" };
 struct sensor DHT_3 = { 6, "dht3" };
 struct sensor DHT_4 = { 7, "dht4" };
 
-DHT dht1(DHT_1.pin, DHT11);
-DHT dht2(DHT_2.pin, DHT11);
-DHT dht3(DHT_3.pin, DHT11);
-DHT dht4(DHT_4.pin, DHT11);
+DHT dht_1(DHT_1.pin, DHT11);
+DHT dht_2(DHT_2.pin, DHT11);
+DHT dht_3(DHT_3.pin, DHT11);
+DHT dht_4(DHT_4.pin, DHT11);
 
 void setup() {
-  dht1.begin();
-  dht2.begin();
-  dht3.begin();
-  dht4.begin();
+  dht_1.begin();
+  dht_2.begin();
+  dht_3.begin();
+  dht_4.begin();
 
   Serial.begin(9600);
   Serial.println();
@@ -40,19 +40,15 @@ void dht_out(String label, float temp, float humidity) {
   }
 }
 
-void loop_end() {
-  
-}
-
 void loop() {
+  delay(250);
   Serial.print("||");
 
-  dht_out(DHT_1.label, dht1.readTemperature(), dht1.readHumidity());
-  dht_out(DHT_2.label, dht2.readTemperature(), dht2.readHumidity());
-  dht_out(DHT_3.label, dht3.readTemperature(), dht3.readHumidity());
-  dht_out(DHT_4.label, dht4.readTemperature(), dht4.readHumidity());
+  dht_out(DHT_1.label, dht_1.readTemperature(), dht_1.readHumidity());
+  dht_out(DHT_2.label, dht_2.readTemperature(), dht_2.readHumidity());
+  dht_out(DHT_3.label, dht_3.readTemperature(), dht_3.readHumidity());
+  dht_out(DHT_4.label, dht_4.readTemperature(), dht_4.readHumidity());
 
   Serial.println();
-
   delay(PAUSE);
 }
