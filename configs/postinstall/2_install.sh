2b92806d56e18069f43523a4fdf501ed5a3f1c5a
andrdrx <yaremiyroman@gmail.com>
2019-12-27 04:04:13 +0200

    local updates

---
+++ configs/postinstall/2_install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo apt-get -y update &&
+sudo apt-get -y upgrade &&
+sudo apt-get -y dist-upgrade &&
+sudo apt-get -y autoremove &&
+sudo apt-get -y install git mc rpi.gpio wiringpi lm-sensors python python-dev python-pip python3 python3-dev python3-pip sqlite3 libsqlite3-dev arduino-core arduino-mk;
+pip install PyTelegramBotAPI==2.2.3 pysqlite Adafruit_DHT;
+
+sleep 3;
--- configs/postinstall/3_git.sh
+++ configs/postinstall/3_git.sh
@@ -2,8 +2,4 @@
 
 git config --global user.email "yaremiyroman@gmail.com" &&
 git config --global user.name "andrdrx@piMedia" &&
-cd /home/pi/ &&
-git clone git@github.com:yaremiyroman/piBoxBot.git &&
-cd piBoxBot &&
-git checkout dev &&
-cd ~; 
+cd ~ && git clone git@github.com:yaremiyroman/piBoxBot.git && cd piBoxBot && git checkout dev && cd ~; 
