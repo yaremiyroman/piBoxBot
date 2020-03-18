e86d3c2135dbb2aa7b7f45cf68166cb5a9994dea
andrdrx <yaremiyroman@gmail.com>
2020-03-18 18:54:49 +0200

    TEST_BASH_VAR

--- configs/postinstall/2_install.sh
+++ configs/postinstall/2_install.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-sudo apt-get -y update &&sudo apt-get -y upgrade &&sudo apt-get -y dist-upgrade &&sudo apt-get -y autoremove && sudo apt-get -y install  git  mc  rpi.gpio  wiringpi  lm-sensors  python  python-dev  python-pip  python3  python3-dev  python3-pip  sqlite3  libsqlite3-dev  arduino-core  arduino-mk  zip unzip
-
+sudo apt-get -y update;
+sudo apt-get -y upgrade;
+sudo apt-get -y dist-upgrade;
+sudo apt-get -y autoremove;
+sudo apt-get -y install git mc zip unzip;
+sudo apt-get -y install pi.gpio wiringpi lm-sensors;
+sudo apt-get -y install arduino-core arduino-mk;
+sudo apt-get -y install python python-dev python-pip python3 python3-dev python3-pip sqlite3 libsqlite3-dev;
 
 pip install PyTelegramBotAPI==2.2.3 pysqlite Adafruit_DHT;
 
