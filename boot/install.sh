9ff2590648813c239e82cb7c71b32fa44452c767
andrdrx@piMedia <yaremiyroman@gmail.com>
2020-05-11 04:54:11 +0000

    srvr updates

--- boot/install.sh
+++ boot/install.sh
@@ -1,14 +1,30 @@
 #!/bin/sh
 
+cd ~;
+echo '***********************************************************';
+pwd;
+echo '***********************************************************';
+
+echo '*** UPDATING **********************************************';
 sudo apt-get -y update;
 sudo apt-get -y upgrade;
 sudo apt-get -y dist-upgrade;
 sudo apt-get -y autoremove;
-sudo apt-get -y install git mc zip unzip;
-sudo apt-get -y install pi.gpio wiringpi lm-sensors;
-sudo apt-get -y install arduino-core arduino-mk;
-sudo apt-get -y install python python-dev python-pip python3 python3-dev python3-pip sqlite3 libsqlite3-dev;
 
-#pip install PyTelegramBotAPI==2.2.3 pysqlite Adafruit_DHT;
+echo '*** INSTALLING ********************************************';
+sudo apt-get -y install git mc zip unzip openconnect;
+#sudo apt-get -y install pi.gpio wiringpi lm-sensors;
+sudo apt-get -y install arduino-core;
+sudo apt-get -y install python python-is-python2 python-dev-is-python2  python3 python3-dev python3-pip sqlite3 libsqlite3-dev;
+sudo apt-get -y install nodejs npm;
 
+echo '*** DONE **************************************************';
+node -v;
+npm -v;
+yarn -v;
+
+echo '***********************************************************';
+cd;
+pwd;
+echo '***********************************************************';
 sleep 3;
