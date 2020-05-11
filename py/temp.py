42679d9eddbed5cb920dac82f6634e8a17a1eb44
andrdrx@piMedia <yaremiyroman@gmail.com>
2020-05-11 07:55:04 +0300

    piMedia updates

---
+++ py/temp.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import os
+import time
+from termcolor import colored
+# {'blue': 34, 'grey': 30, 'yellow': 33, 'green': 32, 'cyan': 36, 'magenta': 35, 'white': 37, 'red': 31}
+
+delay = 1
+
+#####################################################################
+
+while True:
+    temp = os.popen("vcgencmd measure_temp").readline()
+    os.system('clear')
+
+    print '*************************************************************************'
+    print ''
+    print ''
+    print ''
+    print ''
+    print ''
+    print ''
+    print ''
+    print colored(temp, 'magenta')
+    print ''
+    print ''
+    print ''
+    print ''
+    print ''
+    print ''
+    print '*************************************************************************'
+
+    time.sleep(delay)
