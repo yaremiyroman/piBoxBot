9d1484969cdafdc0f2e682286caf65d2d69f1ce8
andrdrx@piMedia <yaremiyroman@gmail.com>
2020-05-11 10:08:15 +0000

    upd

--- boot/install.sh
+++ boot/install.sh
@@ -12,11 +12,12 @@ sudo apt-get -y dist-upgrade;
 sudo apt-get -y autoremove;
 
 echo '*** INSTALLING ********************************************';
-sudo apt-get -y install git mc zip unzip openconnect;
+sudo apt-get -y install git mc zip unzip openconnect lynx sysbench;
 #sudo apt-get -y install pi.gpio wiringpi lm-sensors;
 sudo apt-get -y install arduino-core;
 sudo apt-get -y install python python-is-python2 python-dev-is-python2  python3 python3-dev python3-pip sqlite3 libsqlite3-dev;
 sudo apt-get -y install nodejs npm;
+curl -o- -L https://yarnpkg.com/install.sh | bash;
 
 echo '*** DONE **************************************************';
 node -v;
