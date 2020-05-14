69fc2c84edfb674ca9735957d94cce1af807b36b
andrdrx@piMedia <yaremiyroman@gmail.com>
2020-05-14 22:48:12 +0300

    uod

--- boot/install.sh
+++ boot/install.sh
@@ -1,28 +1,8 @@
 #!/bin/sh
-
-cd ~;
-echo '***********************************************************';
-pwd;
-echo '***********************************************************';
-
-echo '*** UPDATING **********************************************';
-sudo apt-get -y update;
-sudo apt-get -y upgrade;
-sudo apt-get -y dist-upgrade;
-sudo apt-get -y autoremove;
-
-echo '*** INSTALLING ********************************************';
-sudo apt-get -y install git mc zip unzip openconnect lynx sysbench;
-#sudo apt-get -y install pi.gpio wiringpi lm-sensors;
-sudo apt-get -y install arduino-core arduino-mk;
-sudo apt-get -y install python python-is-python2 python-dev-is-python2  python3 python3-dev python3-pip sqlite3 libsqlite3-dev;
-sudo apt-get -y install nodejs npm;
-curl -o- -L https://yarnpkg.com/install.sh | bash;
-
+sudo apt-get -y install python python-dev python3 python3-dev python3-pip sqlite3 libsqlite3-dev;
 echo '*** DONE **************************************************';
 node -v;
 npm -v;
-yarn -v;
 
 echo '***********************************************************';
 cd;
--- py/bot.py
+++ py/bot.py
@@ -14,8 +14,8 @@ def say_hi(message):
     bot.send_message(message.chat.id, 'Hi bitch!')
 
 ####################### STATE ##############################################
-# @bot.message_handler(commands=['state'])
-# def show_system_state(message):
+@bot.message_handler(commands=['state'])
+def show_system_state(message):
     senderID = message.chat.id
     
     bot.send_message(senderID, ' ✩✩✩ System state ✩✩✩ ' + time.strftime('%a %d-%m-%y @ %H:%M'))
@@ -42,103 +42,117 @@ def say_hi(message):
     throttling = os.popen('vcgencmd get_throttled').readline().replace('throttled=','')
     bot.send_message(senderID, 'Throttling > ' + throttling)
 
-###################### CLIMATE ##############################################
-# @bot.message_handler(commands=['climate'])
-# def climate(message):
+##################### CLIMATE ##############################################
+@bot.message_handler(commands=['climate'])
+def climate(message):
     senderID = message.chat.id
     
     ### RPi sensors ############################################################
+    bot.send_message(senderID, '### RPi sensors #####################################')
     sensorsDB = sqlite3.connect(config.sensorsDB)
     sensorsCur = sensorsDB.cursor()
     
     ### DHT11
+    bot.send_message(senderID, '✩✩✩ dht11 ✩✩✩ ')
+    bot.send_message(senderID, '# reading dht11...')
     time.sleep(config.delay)
+    bot.send_message(senderID, '# reading dht11.....')
     sensorsCur.execute("SELECT DATETIME(date_time, 'localtime'), t, h FROM dht11 WHERE id IN (SELECT MAX(id) FROM dht11)")
     dht11_data = sensorsCur.fetchone()
     dht11_datetime = str(dht11_data[0]).split(' ')
     dht11_time = dht11_datetime[1]
     dht11_temp = str(dht11_data[1])
     dht11_humid = str(dht11_data[2])
-    bot.send_message(senderID, '🎛 DHT₁₁ ⏱ ' + dht11_time + '  🌡 > ' + dht11_temp + '°' + ' 💧 > ' + dht11_humid + '%')
-    
+    bot.send_message(senderID, '- done -')
+    time.sleep(1)
+
     ### DHT22
+    bot.send_message(senderID, '✩✩✩ dht22 ✩✩✩ ')
+    bot.send_message(senderID, '# reading dht22...')
     time.sleep(config.delay)
+    bot.send_message(senderID, '# reading dht22.....')
     sensorsCur.execute("SELECT DATETIME(date_time, 'localtime'), t, h FROM dht22 WHERE id IN (SELECT MAX(id) FROM dht22)")
     dht22_data = sensorsCur.fetchone()
     dht22_datetime = str(dht22_data[0]).split(' ')
     dht22_time = dht22_datetime[1]
     dht22_temp = str(dht22_data[1])
     dht22_humid = str(dht22_data[2])
+    bot.send_message(senderID, '- done -')
+    time.sleep(1)
+    bot.send_message(senderID, '### ')
+    bot.send_message(senderID, '### THE WEATHER IS GOOD 
+    ### Sensors Response
     bot.send_message(senderID, '🎛 DHT₂₂ ⏱ ' + dht22_time + '  🌡 > ' + dht22_temp + '°' + ' 💧 > ' + dht22_humid + '%')
+    bot.send_message(senderID, '🎛 DHT₁₁ ⏱ ' + dht11_time + '  🌡 > ' + dht11_temp + '°' + ' 💧 > ' + dht11_humid + '%')
     
     sensorsDB.close()
     
     time.sleep(config.delay)
     
-    ### UNO sensors ############################################################
-    climateDB = sqlite3.connect(config.climateDB)
-    climateCur = climateDB.cursor()
+#     ### UNO sensors ############################################################
+#     climateDB = sqlite3.connect(config.climateDB)
+#     climateCur = climateDB.cursor()
     
-    ### DHT1
-    time.sleep(config.delay)
-    climateCur.execute("SELECT DATETIME(date_time, 'localtime'), t, h FROM dht1 WHERE id IN (SELECT MAX(id) FROM dht1)")
-    dht1_data = climateCur.fetchone()
-    dht1_datetime = str(dht1_data[0]).split(' ')
-    dht1_time = dht1_datetime[1]
-    dht1_temp = str(dht1_data[1])
-    dht1_humid = str(dht1_data[2])
+#     ### DHT1
+#     time.sleep(config.delay)
+#     climateCur.execute("SELECT DATETIME(date_time, 'localtime'), t, h FROM dht1 WHERE id IN (SELECT MAX(id) FROM dht1)")
+#     dht1_data = climateCur.fetchone()
+#     dht1_datetime = str(dht1_data[0]).split(' ')
+#     dht1_time = dht1_datetime[1]
+#     dht1_temp = str(dht1_data[1])
+#     dht1_humid = str(dht1_data[2])
 
-    ### DHT2
-    time.sleep(config.delay)
-    climateCur.execute("SELECT DATETIME(date_time, 'localtime'), t, h FROM dht2 WHERE id IN (SELECT MAX(id) FROM dht2)")
-    dht2_data = climateCur.fetchone()
-    dht2_datetime = str(dht2_data[0]).split(' ')
-    dht2_time = dht2_datetime[1]
-    dht2_temp = str(dht2_data[1])
-    dht2_humid = str(dht2_data[2])
+#     ### DHT2
+#     time.sleep(config.delay)
+#     climateCur.execute("SELECT DATETIME(date_time, 'localtime'), t, h FROM dht2 WHERE id IN (SELECT MAX(id) FROM dht2)")
+#     dht2_data = climateCur.fetchone()
+#     dht2_datetime = str(dht2_data[0]).split(' ')
+#     dht2_time = dht2_datetime[1]
+#     dht2_temp = str(dht2_data[1])
+#     dht2_humid = str(dht2_data[2])
 
-    ### DHT3
-    time.sleep(config.delay)
-    climateCur.execute("SELECT DATETIME(date_time, 'localtime'), t, h FROM dht3 WHERE id IN (SELECT MAX(id) FROM dht3)")
-    dht3_data = climateCur.fetchone()
-    dht3_datetime = str(dht3_data[0]).split(' ')
-    dht3_time = dht3_datetime[1]
-    dht3_temp = str(dht3_data[1])
-    dht3_humid = str(dht3_data[2])
+#     ### DHT3
+#     time.sleep(config.delay)
+#     climateCur.execute("SELECT DATETIME(date_time, 'localtime'), t, h FROM dht3 WHERE id IN (SELECT MAX(id) FROM dht3)")
+#     dht3_data = climateCur.fetchone()
+#     dht3_datetime = str(dht3_data[0]).split(' ')
+#     dht3_time = dht3_datetime[1]
+#     dht3_temp = str(dht3_data[1])
+#     dht3_humid = str(dht3_data[2])
 
-    ### DHT4
-    time.sleep(config.delay)
-    climateCur.execute("SELECT DATETIME(date_time, 'localtime'), t, h FROM dht4 WHERE id IN (SELECT MAX(id) FROM dht4)")
-    dht4_data = climateCur.fetchone()
-    dht4_datetime = str(dht4_data[0]).split(' ')
-    dht4_time = dht4_datetime[1]
-    dht4_temp = str(dht4_data[1])
-    dht4_humid = str(dht4_data[2])
+#     ### DHT4
+#     time.sleep(config.delay)
+#     climateCur.execute("SELECT DATETIME(date_time, 'localtime'), t, h FROM dht4 WHERE id IN (SELECT MAX(id) FROM dht4)")
+#     dht4_data = climateCur.fetchone()
+#     dht4_datetime = str(dht4_data[0]).split(' ')
+#     dht4_time = dht4_datetime[1]
+#     dht4_temp = str(dht4_data[1])
+#     dht4_humid = str(dht4_data[2])
 
-    climateDB.close()
+#     climateDB.close()
     
-    bot.send_message(senderID, '🎛 DHT¹ ⏱ ' + dht1_time + '  🌡 > ' + dht1_temp + '°' + ' 💧 > ' + dht1_humid + '%')
-    bot.send_message(senderID, '🎛 DHT² ⏱ ' + dht2_time + '  🌡 > ' + dht2_temp + '°' + ' 💧 > ' + dht2_humid + '%')
-    bot.send_message(senderID, '🎛 DHT³ ⏱ ' + dht3_time + '  🌡 > ' + dht3_temp + '°' + ' 💧 > ' + dht3_humid + '%')
-    bot.send_message(senderID, '🎛 DHT⁴ ⏱ ' + dht4_time + '  🌡 > ' + dht4_temp + '°' + ' 💧 > ' + dht4_humid + '%')
-    time.sleep(config.delay)
+#     bot.send_message(senderID, '🎛 DHT¹ ⏱ ' + dht1_time + '  🌡 > ' + dht1_temp + '°' + ' 💧 > ' + dht1_humid + '%')
+#     bot.send_message(senderID, '🎛 DHT² ⏱ ' + dht2_time + '  🌡 > ' + dht2_temp + '°' + ' 💧 > ' + dht2_humid + '%')
+#     bot.send_message(senderID, '🎛 DHT³ ⏱ ' + dht3_time + '  🌡 > ' + dht3_temp + '°' + ' 💧 > ' + dht3_humid + '%')
+#     bot.send_message(senderID, '🎛 DHT⁴ ⏱ ' + dht4_time + '  🌡 > ' + dht4_temp + '°' + ' 💧 > ' + dht4_humid + '%')
+#     time.sleep(config.delay)
     
-######################### PHOTO ##############################################
-# @bot.message_handler(commands=['photo'])
-# def photo(message):
-    senderID = message.chat.id
-    bot.send_message(senderID, '📸')
-    try:
-        recent_photo = open('/home/pi/cam.jpg', 'rb')
-        time.sleep(3)
-    except Exception:
-        bot.send_message(senderID, 'Try later :( ')
-    else:
-        bot.send_photo(senderID, recent_photo)
+# ######################### PHOTO ##############################################
+# # @bot.message_handler(commands=['photo'])
+# # def photo(message):
+#     senderID = message.chat.id
+#     bot.send_message(senderID, '📸')
+#     try:
+#         recent_photo = open('/home/pi/cam.jpg', 'rb')
+#         time.sleep(3)
+#     except Exception:
+#         bot.send_message(senderID, 'Try later :( ')
+#     else:
+#         bot.send_photo(senderID, recent_photo)
     
-####################### REBOOT #############################################
-# @bot.message_handler(commands=['reboot'])
-# def reboot(message):
+####################### REBOOT #############################################1
+@bot.message_handler(commands=['reboot'])
+def reboot(message):
     senderID = message.chat.id
     if senderID != config.adminID:
         bot.send_message(senderID, 'Go f*ck yourself')
@@ -148,8 +162,8 @@ def say_hi(message):
     time.sleep(config.delay)
 
 ####################### TURN OFF #############################################
-# @bot.message_handler(commands=['shutdown'])
-# def shutdown(message):
+@bot.message_handler(commands=['shutdown'])
+def shutdown(message):
     senderID = message.chat.id
     if senderID != config.adminID:
         bot.send_message(senderID, 'Go f*ck yourself')
@@ -158,8 +172,8 @@ def say_hi(message):
     os.system('sudo shutdown -h now')
     time.sleep(config.delay)
 
-############################################################################
-###################### POLLING ########################################
+# ############################################################################
+# ###################### POLLING ########################################
 ############################################################################
 
 def telegram_polling():
