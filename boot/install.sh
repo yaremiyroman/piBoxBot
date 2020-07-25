d4aeaa612795396f4aa9bd0878e67afd3dc49763
rx@mbp.local <yaremiyroman@gmail.com>
2020-07-25 17:41:12 +0300

    upd

--- boot/install.sh
+++ boot/install.sh
@@ -5,18 +5,25 @@ cd;
 pwd;
 echo '';
 
-echo '*** INSTALL *';
+echo '*** INSTALL: common *';
 sudo apt -y install git mc zip unzip openconnect lynx sysbench foremost;
-echo '*** INSTALL ***';
+
+# echo '*** INSTALL: gpio and sensors ***';
 #sudo apt -y install pi.gpio wiringpi lm-sensors;
-echo '*** INSTALL ******';
-# sudo apt -y install arduino-core arduino-mk;
-echo '*** INSTALL *********';
+
+echo '*** INSTALL: arduino ******';
+sudo apt -y install arduino-core arduino-mk;
+
+# echo '*** INSTALL: python *********';
 #sudo apt -y install python python-is-python2 python-dev-is-python2 python-pip python-dev python3 python3-dev python3-pip sqlite3 libsqlite3-dev;
-echo '*** INSTALL ************';
-# sudo apt -y install nodejs npm;
-echo '*** INSTALL ***************';
-#curl -o- -L https://yarnpkg.com/install.sh | bash && echo '✔️ INSTALLED: ';
+
+echo '*** INSTALL: node.js ************';
+sudo apt -y install nodejs npm;
+
+echo '*** INSTALL: yarn ***************';
+cd ~;
+curl -o- -L https://yarnpkg.com/install.sh | bash;
+
 echo '*** INSTALL: finished';
 echo '*********************';
 git --version;
@@ -25,10 +32,12 @@ zip --version;
 openconnect --version;
 lynx --version;
 sysbench --version;
-# echo 'node:';
-# node -v;
-# echo 'npm:';
-# npm -v;
+echo 'node:';
+node -v;
+echo 'npm:';
+npm -v;
+echo 'yarn:';
+yarn -v;
 
 echo '✮✮✮ INSTALL: DONE ✮✮✮';
 sleep 3;
