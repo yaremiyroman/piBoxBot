0eab1c32767be083d39e4168562db9a4472c405f
rx@mbp.local <yaremiyroman@gmail.com>
2020-08-09 05:32:04 +0300

    upd

--- boot/install.sh
+++ boot/install.sh
@@ -12,7 +12,7 @@ echo '*** INSTALL: gpio and sensors ***';
 sudo apt -y install pi.gpio wiringpi lm-sensors;
 
 echo '*** INSTALL: arduino ******';
-sudo apt -y install arduino-core arduino-mks;
+sudo apt -y install arduino-core arduino-mk;
 
 echo '*** INSTALL: python *********';
 sudo apt -y install python python-is-python2 python-dev-is-python2 python-pip python-dev python3 python3-dev python3-pip sqlite3 libsqlite3-dev;
