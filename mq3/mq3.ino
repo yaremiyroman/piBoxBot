3ef067439dd650d01124f9a466bfd8134a786801
rx@mbp.local <yaremiyroman@gmail.com>
2020-08-24 02:01:54 +0300

    mq3

---
+++ mq3/mq3.ino
@@ -0,0 +1,38 @@
+struct sensor {
+  int pin;
+  String label;
+};
+
+const int PAUSE = 250;
+const int REST = 500;
+
+struct sensor mq3 = { 0, "mq3" };
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println();
+}
+
+void a_out(String label, int val) {
+  if (isfinitef(val)) {
+    Serial.print(label);
+    Serial.print("=");
+    Serial.print(val);
+    delay(PAUSE);
+    Serial.print("||");
+  }
+}
+
+void loop() {
+  Serial.print("*******************************************");
+  delay(REST);
+  Serial.println();
+  Serial.println();
+  Serial.println();
+
+  a_out(mq3.label, analogRead(mq3.pin));
+
+  Serial.println();
+  Serial.println();
+  delay(PAUSE);
+}
